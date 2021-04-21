51544dff3d413b008d061f7459afab28f48d27d7
rhammer35 <rhammer35@icloud.com>
2021-04-20 22:11:16 -0400

    populated user login with input fields for username and password and created event handler functions for form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,32 +1,32 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
-import { Container, Typography, makeStyles } from '@material-ui/core';
+import { Container, TextField, makeStyles, Button, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  linkText: {
-    textDecoration: `none`,
-    color: 'black'
+  loginForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly'
   }
 });
 
 export default function Login() {
 
+  const history = useHistory();
+  const classes = useStyles();
+
   const initialFormValues = {
-    credentials: {
-      username: '',
-      password: ''
-    }
+    username: '',
+    password: ''
   };
   
-  const [loginCredentials, setLoginCredentials] = useState(initialFormValues);
+  const [credentials, setCredentials] = useState(initialFormValues);
   
   const handleChange = e => {
-    setLoginCredentials({
-      credentials: {
-        ...loginCredentials,
-        [e.target.name]: e.target.value
-      }
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
     });
   };
   
@@ -34,23 +34,47 @@ export default function Login() {
     e.preventDefault();
     // Make a POST request with the username and password as the data body
     axiosWithAuth()
-      .post('/api/login', loginCredentials)
+      .post('/api/login', credentials)
       .then( (res) => {
         // res.data.payload
         // store the token in localStorage (sessions, cookies)...add window in front to accommodate some older browsers
         window.localStorage.setItem('token', JSON.stringify(res.data.payload));
         // navigate to some landing/profile/dashboard page
-        // useHistory('/friends');
-  
+        history.push('/friends');
       })
   };
 
-  const classes = useStyles();
   return (
     <>
-      <Container maxWidth='lg'>
-
-      </Container>
+      <form onSubmit={login} >
+        <Grid container='true' alignContent="space-evenly" alignItems="center" justify="center" direction="column">
+          <Grid item>
+            <TextField
+              name="username"
+              label="Username"
+              type="text"
+              value={credentials.username}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              name="standard-password-input"
+              label="Password"
+              type="password"
+              value={credentials.password}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Button 
+            variant='contained'
+            color='primary'
+            type='submit'
+            >
+            Submit
+          </Button>
+        </Grid>
+      </form>
     </>
   )
 }
